19957fb101afad8fe47741fc30b90105bf3540c8
jswh <jswh@jswh.me>
2016-11-28 11:31:40 +0800

    make vscode project file

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-import {createServer} from 'http';
+/// <reference path="./typings/globals/node/index.d.ts" />
+import * as http from 'http';
+http.createServer((request, response) => {
+    console.log(request.httpVersion);
+    response.write("ok");
+    response.end();
+}).listen(3000, '127.0.0.1');
