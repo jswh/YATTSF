eafb37e5d2052c262e320d16a7867b68a5e91a75
jswh <jswh@jswh.me>
2016-11-29 19:18:06 +0800

    add basic router structure

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 /// <reference path="./typings/globals/node/index.d.ts" />
 import * as http from 'http';
-http.createServer((request, response) => {
-    console.log(request.httpVersion);
-    response.write("ok");
-    response.end();
+const routers = {
+    'post': {
+        '/': (req: http.IncomingMessage, res: http.ServerResponse) => {res.write('Hello World !')}
+    },
+    'get': {
+
+    },
+    'default': (req: http.IncomingMessage, res: http.ServerResponse) => {res.writeHead(404);}
+}; 
+http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    let found = false;
+    for (let path in routers[req.method.toLowerCase()]) {
+        
+    }
+    if (!found && typeof(routers['default']) == 'function') {
+        routers.default(req, res);
+    }
+    res.end();
 }).listen(3000, '127.0.0.1');
